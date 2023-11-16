4a52da4300bef54a2d5abc71abef44f82a4222d2
griler <thienloc662001@gmail.com>
2023-11-16 17:57:16 +0700

    update file

---
+++ demo/test.js
@@ -0,0 +1,10 @@
+
+var twoSum = function(nums, target) {
+    let key
+    const map = new Map()
+    nums.forEach((e,idx) =>
+        console.log(e)
+    )
+};
+let arr1 = [1, 2, 4, 5]
+const demo = twoSum(arr1,10)
