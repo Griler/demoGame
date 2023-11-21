c50b9380ed12b04041910c94da997c0838d9a4c0
griler <thienloc662001@gmail.com>
2023-11-21 16:00:57 +0700

    update folder and file week2-day2

--- demo/EX1/bai4.js
+++
@@ -1,5 +0,0 @@
-function getExtensionFile(a){
-    let arr = a.split('.')
-    console.log(arr[1])
-}
-const bai4 = getExtensionFile('image.png')
--- demo/EX2/bai2.js
+++
@@ -1,5 +0,0 @@
-function randomBetween(min, max) { // min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
-const rndInt = randomBetween(1, 6)
-console.log(rndInt)
